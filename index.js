1c72ef9651d31fa684fb9b45040c16ef6ab96ee2
matt <mlryckman27@gmail.com>
2021-06-03 17:31:54 -0400

    Updated VoteMap class with getters/setters for maintaining data privacy.

--- index.js
+++ index.js
@@ -16,30 +16,36 @@ bot.on('ready', () => {
 let vote = new VoteMap();
 
 bot.on('message', msg => {
-    if (msg.content === '!startvote' && vote.voteStatus === false) {
+    if (msg.content === '!startvote' && vote.getVoteStatus === false) {
         msg.channel.send('Please vote for one of the four available maps using the commands: "!skeld", "!mirahq", "!polus", or "!airship" (without the ""). Please only vote once.\n');
-
         vote.startVote();
     }
-    else if (msg.content === '!skeld' && vote.voteStatus === true) {
+    else if (msg.content === '!skeld' && vote.getVoteStatus === true) {
         msg.channel.send(`${msg.author.tag} voted for Skeld.`);
         vote.incSkeld();
     }
-    else if (msg.content === '!mirahq' && vote.voteStatus === true) {
+    else if (msg.content === '!mirahq' && vote.getVoteStatus === true) {
         msg.channel.send(`${msg.author.tag} voted for MiraHQ.`);
         vote.incMirahq();
     }
-    else if(msg.content === '!polus' && vote.voteStatus === true) {
+    else if(msg.content === '!polus' && vote.getVoteStatus === true) {
         msg.channel.send(`${msg.author.tag} voted for Polus.`);
         vote.incPolus();
     }
-    else if (msg.content === '!airship' && vote.voteStatus === true) {
+    else if (msg.content === '!airship' && vote.getVoteStatus === true) {
         msg.channel.send(`${msg.author.tag} voted for Airship.`);
         vote.incAirship();
     }
-    else if (msg.content === '!results' && vote.voteStatus === true) {
+    else if (msg.content === '!results' && vote.getVoteStatus === true) {
         let finalCount = vote.results();
         msg.channel.send(finalCount);
     }
-    
+    else if (msg.content === '!help') {
+        msg.channel.send("Commands: \n \
+        !help - Displays this help message.\n \
+        !startvote - Starts a new round of voting.\n \
+        ![skeld, mirahq, polus, airship] - Sends one vote for specified map. Don't include the [ ] symbols when entering your map selection.\n \
+        !results - Display the results of the vote, then reset all counts to zero.\n");
+    }
+
 })
--- votemap.js
+++ votemap.js
@@ -1,6 +1,4 @@
 class VoteMap {
-
-    // TODO: add getters and setters using get/set keywords and implement the methods into the main index.js where needed
     
     constructor() {
         this.skeld = 0;
@@ -11,40 +9,146 @@ class VoteMap {
         this.voteStatus = false;
     }
 
+    // Getters return number of votes for specific map.
+
+    /**
+     * @returns {number}: number of votes for Skeld.
+     */
+    get skeldCount() {
+
+        return this.skeld;
+    }
+    
+    /**
+     * @returns {number}: number of votes for MiraHQ.
+     */
+    get mirahqCount() {
+
+        return this.mirahq;
+    }
+
+    /**
+     * @returns {number}: number of votes for Polus.
+     */
+    get polusCount() {
+        return this.polus;
+    }
+
+    /**
+     * @returns {number}: number of votes for Airship.
+     */
+    get airshipCount() {
+        return this.airship;
+    }
+
+    /**
+     * @returns {boolean}: If true, voting is in progress; false, if voting not in progress.
+     */
+    get getVoteStatus() {
+        return this.voteStatus;
+    }
+
+
+    
+    // Setters assign a vote count to a specific map.
+
+    /**
+     * @param {number} {votes}: Number of votes to set for Skeld.
+     */
+    set setSkeldCount(votes) {
+        this.skeld = votes;
+    }
+
+    /**
+     * @param {number} {votes}: Number of votes to set for MiraHQ.
+     */
+    set setMirahqCount(votes) {
+        this.mirahq = votes;
+    }
+
+    /**
+     * @param {number} {votes}: Number of votes to set for Polus.
+     */
+    set setPolusCount(votes) {
+        this.polus = votes;
+    }
+
+    /**
+     * @param {number} {votes}: Number of votes to set for Airship.
+     */
+    set setAirshipCount(votes) {
+        this.airship = votes;
+    }
+
+    /**
+     * @param {boolean} {status}: true sets voteStatus; false unsets voteStatus.
+     */
+    set setVoteStatus(status) {
+        this.voteStatus = status;
+    }
+
+
+    /**
+     * @returns {boolean}: Set voteStatus to true to start the vote.
+     */
     startVote() {
-        this.voteStatus = true;
+        this.setVoteStatus = true;
     }
 
+    /**
+     * Increment the vote count for Skeld by 1.
+     */
     incSkeld() {
-        this.skeld++;;
+        this.skeld++;
     }
 
+    /**
+     * Increment the vote count for MiraHQ by 1.
+     */
     incMirahq() {
         this.mirahq++;
     }
 
+    /**
+     * Increment the vote count for Polus by 1.
+     */
     incPolus() {
         this.polus++;
     }
 
+    /**
+     * Increment the vote count for Airship by 1.
+     */
     incAirship() {
         this.airship++;
     }
 
+    /**
+     * Reset the vote for all maps to zero.
+     * This method is called whenever the results() 
+     * method is called, so the vote can be reset
+     * for next round.
+     */
     clearVote() {
-        this.skeld = 0;
-        this.mirahq = 0;
-        this.polus = 0;
-        this.airship = 0;
+        this.setSkeldCount = 0;
+        this.setMirahqCount = 0;
+        this.setPolusCount = 0;
+        this.setAirshipCount = 0;
     }
 
+    /**
+     * This method is called when voting is finished 
+     * and votes need to be tallied.
+     * @returns {string}: A string containing the final vote for each map.
+     */
     results() {
-        const result = 'Skeld: ' + this.skeld + '\n' + 'MiraHQ: ' + this.mirahq + '\n' + 'Polus: ' + this.polus + '\n' + 'Airship: ' + this.airship + '\n';
+        const result = 'Skeld: ' + this.skeldCount + '\n' + 'MiraHQ: ' + this.mirahqCount + '\n' + 'Polus: ' + this.polusCount + '\n' + 'Airship: ' + this.airshipCount + '\n';
         this.clearVote();
-        this.voteStatus = false;
+        this.setVoteStatus = false;
         return result;
 
     }
+
 }
 
 module.exports = VoteMap;
